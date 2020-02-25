2e282841532b7006e9250c077cb6257c2d16636b
katrin-kot <enkor89@mail.ru>
2020-02-25 12:49:41 +0300

    count-cats is done

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,15 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  //тебе же придти что-то должно, а ничего не приходило
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    // matrix[0] - это массив, так? да
+    const current = matrix[i]; //чтобы не путаться сохраним в отдельтную переменную
+    // current - это массив ['##', 'dd', '00']
+    for (let j = 0; j < current.length; j++) { //а откуда среда тебе высрет array?
+      if (current[j] === '^^') {
+        count = +1; // кто тебя учил ретурн в цикле писать? 
+      }
+    }
+  }
+  return count;
 };
